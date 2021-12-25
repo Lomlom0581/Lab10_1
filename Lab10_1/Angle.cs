1f91c7fff36e644439a122a2ce0f0fd5bf35db54
Антон Еремеев <kzanton_81@mail.ru>
2021-12-25 21:57:42 +0300

    Добавьте файлы проекта.

---
+++ Lab10_1/Angle.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_1
+{
+    public class Angle
+    {
+        
+        private int _degree; 
+        private int _min;
+        private int _sec;
+                
+        #region Properties
+                
+        public int Gradus
+        {
+            get { return _degree; }
+            set
+            {
+                if (value > 360 || value < 0) throw new Exception("Неверный угол. Должен быть 0-360");
+                _degree = value;
+            }
+        }
+
+        public int Min
+        {
+            get { return _min; }
+            set
+            {
+                if (value > 60 || value < 0) throw new Exception("Неверный угол. минуты должны быть 0-60");
+                _min = value;
+            }
+        }
+
+        public int Sec
+        {
+            get { return _sec; }
+            set
+            {
+                if (value > 60 || value < 0) throw new Exception("Неверный угол. секунды должны быть 0-60");
+                _sec = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+               
+        public Angle()
+        {
+
+        }
+
+        public Angle(int Degrees, int Minutes, int Seconds)
+        {
+            Gradus = Degrees;
+            Min = Minutes;
+            Sec = Seconds;
+        }
+
+        public Angle(int Degrees, int Minutes)
+        {
+            Gradus = Degrees;
+            Min = Minutes;
+        }
+
+        public Angle(int Degrees)
+        {
+            Gradus = Degrees;
+        }
+        #endregion
+
+        #region Methods
+        
+        public double ToRad() => (_degree + (_min + _sec / 60) / 60) * Math.PI / 180;
+                
+        public override string ToString()
+        {
+            return _degree + "° " + _min + "' " + _sec + "''";
+        }
+
+        #endregion
+
+    }
+}
---
+++ Lab10_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Программа переводит угол из градусов в радианы.");
+
+            Console.Write("Введите градусы:");
+            int d = int.Parse(Console.ReadLine());
+            Console.Write("Введите минуты:");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите секунды:");
+            int s = int.Parse(Console.ReadLine());
+
+            try
+            {                
+                Angle a = new Angle(d, m, s);
+                Console.WriteLine($"Получилось {a}, это {a.ToRad()} радиан.");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Что-то пошло не так: " + e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
